8dc1477b6801c41f6de0657ad489ea2fd8539b62
Samuel <61188554+samibanks01@users.noreply.github.com>
2024-11-07 18:06:25 -0900

    Create main.py
    Add Files

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Reflection journal 📓")
+
+i = 1
+while day < 31:
+    print("\033[32m", "Day", day)
+    print("\033[0m", "What did you think about the task?")
+    comment= input()
+    summary = f"You thought Day {day} was {comment}"
+    print(f"{summary:^40}")
+    print()
+    day += 1
